a2f4433b52960ed1515376d0779898a117ec174e
Jamerson Siqueira <jamerson.ss16@gmail.com>
2023-09-20 00:29:32 -0300

    v1.0
    Bot basic idea, working on https://taylor-swift-sp.sales.ticketsforfun.com.br/

---
+++ BotSwifter/src/main/java/org/example/main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class main {
+        public static void main(String[] args) {
+            //Chromeinstance padrão
+            ChromeOptions co1 = new ChromeOptions();
+            co1.addArguments("--headless");
+            System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver/chromedriver.exe");
+            WebDriver driver = new ChromeDriver(co1);
+            driver.get("https://taylor-swift-sp.sales.ticketsforfun.com.br/");
+            WebElement spanElement = driver.findElement(By.id("hlLinkToQueueTicket2"));
+            WebElement foo = new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.presenceOfElementLocated(By.id("MainPart_pMessageOnQueueTicket")));
+            String pageText = driver.findElement(By.tagName("body")).getText();
+            System.out.println(pageText);
+            String footer = driver.findElement(By.id("hlLinkToQueueTicket2")).getText();
+            System.out.println(footer);
+            WebElement spanElement2 = driver.findElement(By.id("MainPart_lbQueueNumber"));
+            String textoPosicao = spanElement2.getText();
+            String textoSpan = spanElement.getText();
+            System.out.println("Texto do span do chrome normal: " + textoSpan);
+            System.out.println("Posição na fila do chrome normal: " + textoPosicao);
+
+
+            //Chromeinstance incognito
+            ChromeOptions co = new ChromeOptions();
+            co.addArguments("--incognito");
+            WebDriver chromeIncog = new ChromeDriver(co);
+            chromeIncog.get("https://taylor-swift-sp.sales.ticketsforfun.com.br/");
+            WebElement spanElementIncognito = chromeIncog.findElement(By.id("hlLinkToQueueTicket2"));
+            String textoSpanIncognito = spanElementIncognito.getText();
+            System.out.println("Texto do span do chrome incognito: " + textoSpanIncognito);
+
+            //mozilla
+            System.setProperty("webdriver.gecko.driver", "src/drivers/geckodriver.exe");
+            WebDriver mdriver = new FirefoxDriver();
+            mdriver.get("https://taylor-swift-sp.sales.ticketsforfun.com.br/");
+            WebElement spanElementm = mdriver.findElement(By.id("hlLinkToQueueTicket2"));
+            String textoSpanm = spanElementm.getText();
+            System.out.println("Texto do span do firefox normal: " + textoSpanm);
+
+        }
+    }
---
+++ README.md
@@ -0,0 +1,16 @@
+# Taylor's Swift Queue Bot
+## _A project to get the best place on the ticket sale_
+
+
+Have some friends trying to buy tickets for the highly anticipated Taylor Swift concert, afraid of not being able to get thir tickets, mix that with an hour of development using Selenium...
+
+**✨That's Taylor Swift Queue Bot! ✨**
+
+(PS: I'm just uploading this project just for study purposes, because the website that sales the tickets isn't available anymore)
+
+## Features
+
+- Connects to the website using Chrome (ChromeDriver) and Mozilla Firefox (GeckoDriver)
+- Get your position's on the line and the link to access it from another device!
+
+Maybe I'll update this project to use it in other's artists tours and concerts in this website and to make it more effective!
